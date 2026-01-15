71ed171897a0b5237fe3a3425567fbef99ce0ed2
Mykola <nyr3mvneov@gmail.com>
2026-01-15 14:00:28 +0200

    improve table

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../api';
+import { Person } from '../types/Person';
 import { PeopleFilters } from './PeopleFilters';
-import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => {
+        setErrorMessage('Something went wrong');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -15,15 +35,11 @@ export const PeoplePage = () => {
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
-
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
-
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              <PeopleTable
+                people={people}
+                isLoading={isLoading}
+                errorMessage={errorMessage}
+              />
             </div>
           </div>
         </div>
---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { Person } from '../types/Person';
+
+interface Props {
+  person: Person;
+}
+
+export const PersonLink = ({ person }: Props) => (
+  <Link
+    to={`/people/${person.slug}`}
+    className={classNames({ 'has-text-danger': person.sex === 'f' })}
+  >
+    {person.name}
+  </Link>
+);
